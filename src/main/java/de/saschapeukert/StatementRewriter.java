222bec5155426ef9a71605559908c40773f0b48e
Sascha Peukert <sascha.peukert@gmail.com>
2016-07-03 17:26:19 +0200

    Extracted all Methods to rewrite statements into StatementRewriter class. changed tests accordingly

---
+++ src/main/java/de/saschapeukert/StatementRewriter.java
@@ -0,0 +1,88 @@
+package de.saschapeukert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Sascha Peukert on 03.07.2016.
+ */
+public class StatementRewriter {
+
+    /**
+     * The keywords according to the railroad from https://github.com/opencypher/openCypher that can be encountered after
+     * CREATE PathExpression
+     * Railroad: https://s3.amazonaws.com/artifacts.opencypher.org/railroad/Cypher.html
+     */
+    private static String[] cypherKeywords = {"SET ","DELETE ","REMOVE ","UNWIND ","FOREACH ", "RETURN ", " MATCH ", "WHERE ",
+            "OPTIONAL", "DETACH ", "WITH ", "CREATE ", "MERGE ", ";"};
+
+    public String rewrite(String statement){
+        statement = replaceStringFromStatement(statement,"  "," "); // replace useless whitespace
+        statement = replacePathWithVirtualPaths(statement,extractVirtualPart(statement));
+        // remove VIRTUAL
+        statement = replaceStringFromStatement(statement,"CREATE VIRTUAL ","CREATE ");
+
+        return statement;
+    }
+
+    /**
+     *
+     * @param statement
+     * @param remove Needs to be longer than instead string!
+     * @param instead
+     * @return
+     */
+    public String replaceStringFromStatement(String statement, String remove, String instead){
+        int idx = statement.toLowerCase().indexOf(remove.toLowerCase());
+        while(idx!=-1){
+            statement = statement.substring(0,idx) + instead +statement.substring(idx+remove.length());
+            idx = statement.toLowerCase().indexOf(remove.toLowerCase());
+        }
+
+        return statement;
+    }
+
+    public String replacePathWithVirtualPaths(String statement, List<PathReplacement> listOfPathReplacements){
+        for(int i=listOfPathReplacements.size()-1;i>=0;i--){
+            PathReplacement p  = listOfPathReplacements.get(i);
+            statement = statement.substring(0,p.getStartPos()) + p.getNewPathString()
+                    + statement.substring(p.getEndPos()+1);
+
+        }
+        return statement;
+    }
+
+    public List<PathReplacement> extractVirtualPart(String statement){
+        List<PathReplacement> returnList = new ArrayList<>(); // Multiple Create Virtual possible!
+
+        String createVirtualString = "CREATE VIRTUAL ";
+        String path = "";
+        int diffSum =0;
+
+        while(statement.toUpperCase().contains(createVirtualString)){
+            // find start
+            int posStart = statement.toUpperCase().indexOf(createVirtualString);
+            posStart = posStart + createVirtualString.length();
+            path = statement.substring(posStart); // statement with start after Create virtual
+            // find end of path -> looking for next cypher keyword
+            int pos_end_min = path.length()-1;
+            for(String s:cypherKeywords){
+                if(path.contains(s)) {
+                    int tempPos = path.toUpperCase().indexOf(s) - 1;
+                    if (tempPos < pos_end_min)
+                        pos_end_min = tempPos;
+                }
+            }
+            path = path.substring(0,pos_end_min);
+            returnList.add(new PathReplacement(path,posStart+diffSum));
+            path = createVirtualString + path;
+
+            diffSum = diffSum + statement.length() - (statement.substring(0,statement.toUpperCase().indexOf(path.toUpperCase())) +
+                    statement.substring(statement.toUpperCase().indexOf(path.toUpperCase()) + path.length())).length();
+
+            statement =statement.substring(0,statement.toUpperCase().indexOf(path.toUpperCase())) +
+                    statement.substring(statement.toUpperCase().indexOf(path.toUpperCase()) + path.length());
+        }
+        return returnList;
+    }
+}
---
+++ src/test/de/de/saschapeukert/StatementRewriterTests.java
@@ -0,0 +1,87 @@
+package de.saschapeukert;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created by Sascha Peukert on 03.07.2016.
+ */
+public class StatementRewriterTests {
+
+    private static StatementRewriter sut;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        sut = new StatementRewriter();
+    }
+
+    @Test
+    public void extractVirtualPartShouldWorkProperly(){
+        String count ="MATCH(n) RETURN COUNT(n)";
+        String oneVirtual = "CREATE VIRTUAL (n:Test) RETURN n";
+        String threeVirtual = "CREATE VIRTUAL (n:Test) CREATE VIRTUAL (m:Person) WITH n,m create VIRTUAL m-[:WROTE]->n RETURN n";
+        String oneVirtualoneReal = "CREATE VIRTUAL (n:Test) CREATE (m:Person) RETURN n,m";
+
+        List<PathReplacement> list =sut.extractVirtualPart(count);
+        Assert.assertEquals("Querys without CREATE VIRTUAL should not be appear in the list at all",0,list.size());
+
+        list =sut.extractVirtualPart(oneVirtual);
+        Assert.assertEquals("Failed to recognize Statement: " + oneVirtual,1,list.size());
+        Assert.assertEquals("Failed to extract Statement: " + oneVirtual,"(n:Test)",list.get(0).getOriginalPathString());
+
+        Assert.assertEquals("Wrong Startposition of PathReplacement for (n:Test)",15,list.get(0).getStartPos());
+        Assert.assertEquals("Wrong Endposition of PathReplacement for (n:Test)",22,list.get(0).getEndPos());
+
+        list =sut.extractVirtualPart(threeVirtual);
+        Assert.assertEquals("Failed to recognize Statement: " + threeVirtual,3,list.size());
+        Assert.assertEquals("Failed to extract first path in Statement: " + threeVirtual,"(n:Test)",list.get(0)
+                .getOriginalPathString());
+        Assert.assertEquals("Failed to extract second path in Statement: " + threeVirtual,"(m:Person)",list.get(1)
+                .getOriginalPathString());
+        Assert.assertEquals("Failed to extract third path in Statement: " + threeVirtual,"m-[:WROTE]->n",list.get(2)
+                .getOriginalPathString());
+
+        Assert.assertEquals("Wrong Startposition of PathReplacement for (n:Test)",15,list.get(0).getStartPos());
+        Assert.assertEquals("Wrong Endposition of PathReplacement for (n:Test)",22,list.get(0).getEndPos());
+        Assert.assertEquals("Wrong Startposition of PathReplacement for (m:Person)",39,list.get(1).getStartPos());
+        Assert.assertEquals("Wrong Endposition of PathReplacement for (m:Person)",48,list.get(1).getEndPos());
+        Assert.assertEquals("Wrong Startposition of PathReplacement for m-[:WROTE]->n",74,list.get(2).getStartPos());
+        Assert.assertEquals("Wrong Endposition of PathReplacement for m-[:WROTE]->n",86,list.get(2).getEndPos());
+
+        list =sut.extractVirtualPart(oneVirtualoneReal);
+        Assert.assertEquals("Failed to recognize Statement: " + oneVirtualoneReal,1,list.size());
+        Assert.assertEquals("Failed to extract Statement: " + oneVirtualoneReal,"(n:Test)",list.get(0)
+                .getOriginalPathString());
+    }
+
+    @Test
+    public void removeWhitespaceShouldWorkFine(){
+        String count ="MATCH (n) RETURN COUNT (n)";
+        String blownCount = "MATCH (n)      RETURN    COUNT (n)";
+
+        Assert.assertEquals(count,sut.replaceStringFromStatement(blownCount,"  "," "));
+    }
+
+    @Test
+    public void replacePathWithVirtualPathsShouldWork(){
+        String threeVirtual = "CREATE VIRTUAL (n:Test) CREATE VIRTUAL (m:Person) WITH n,m create VIRTUAL m-[:WROTE]->n RETURN n";
+        List<PathReplacement> list =sut.extractVirtualPart(threeVirtual);
+
+        String result = sut.replacePathWithVirtualPaths(threeVirtual,list);
+        String inject = "{" + BaselineProcedure.PROPERTYKEY + ":true}";
+        String correctResult = "CREATE VIRTUAL (n:Test"+inject+") CREATE VIRTUAL (m:Person"+inject+") WITH n,m create VIRTUAL m-[:WROTE"+inject+"]->n RETURN n";
+
+        Assert.assertEquals(correctResult,result);
+    }
+
+    @Test
+    public void replaceCreateVirtualWithCreateShouldWork(){
+        String teststring ="CREATE (n:Person) WITH n CREATE VIRTUAL (t:TEST) CREATE VIRTUAL n-[:WROTE]->t";
+        String resultString = "CREATE (n:Person) WITH n CREATE (t:TEST) CREATE n-[:WROTE]->t";
+
+        Assert.assertEquals(resultString,sut.replaceStringFromStatement(teststring,"CREATE VIRTUAL ","CREATE "));
+    }
+}
